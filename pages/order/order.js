037dea3650576b953a89377f5e70dc0a82670d9b
LiuYang <liuyang@idami.cc>
2018-05-09 18:30:12 +0800

    增加GPIO输入输出控制
    Signed-off-by: LiuYang <liuyang@idami.cc>

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,8 +1,15 @@
 var app = getApp()      // 获取入口文件app的应用实例
+var tempData = { "status_code": 200, "order_list": [{ "order_id": "20180501001001", "created_at": "2018-05-01 10:00:00", "order_total": "2.6", "order_status": "Finish", "pay_type": "Wechat", "menu_info": [{ "goods_pic": "www.we9pay.com/xx/xx.jpg", "goods_name": "Sprite 250ml", "goods_number": "2", "goods_price": "1.6" }, { "goods_pic": "www.we9pay.com/xx/xx.jpg", "goods_name": "Custard pie", "goods_number": "1", "goods_price": "1.0" }] }, { "order_id": "20180502002002", "created_at": "2018-05-02 10:00:00", "order_total": "2.6", "order_status": "Finish", "pay_type": "Wechat", "menu_info": [{ "goods_pic": "www.we9pay.com/xx/xx.jpg", "goods_name": "Cool 250ml", "goods_number": "2", "goods_price": "1.6" }, { "goods_pic": "www.we9pay.com/xx/xx.jpg", "goods_name": "Custard pie", "goods_number": "1", "goods_price": "1.0" }] }] };
 Page({
   data: {
+    Order: "",
   },
   onLoad: function (options) {
-   
-  },
+    var that = this;
+    wx.setNavigationBarTitle({ title: 'My Order' }),
+    wx.showLoading({ title: 'Order loading', })
+
+    wx.hideLoading();
+    that.setData({ Order: tempData.order_list})
+  }
 })
