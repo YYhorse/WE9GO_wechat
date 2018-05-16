890d0a3730b080c912f125eea561dbc2db04efc3
LiuYang <liuyang@idami.cc>
2018-05-16 16:11:14 +0800

    完成扫码登陆控制，保留了标签弹出框
    Signed-off-by: LiuYang <liuyang@idami.cc>

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ App({
   globalData: {
     code: null,
     user_id: null,
-    userInfo: null
+    userInfo: null,
+    HomeUrl: 'https://www.matrixsci.cn/supermarket',
+    LoginUrl:'/user/login?',
+    OpenDoorUrl:'/store/open?'
   },
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,13 +5,39 @@ Page({
   data: {
     AvatarUrl: "",
     NiceName: "Login...",
+    UsePermission: true
   },
   onLoad: function () {
-    this.获取用户信息();
+    // 查看是否授权
+    var that = this;
+    wx.getSetting({
+      success: function (res) {
+        console.log(res.authSetting);
+        if (res.authSetting['scope.userInfo']) {
+          that.setData({UsePermission:true})
+          that.获取用户信息();
+        }
+        else{
+          wx.showToast({ title: "Access to user information failure!", })
+          that.setData({ UsePermission: false })
+        }
+      }
+    })
+  },
+  bindGetUserInfo: function (e) {
+    console.log(e.detail.userInfo)
+    if (e.detail.userInfo) {
+      this.setData({ UsePermission: true })
+      this.获取用户信息();
+    } else {
+     wx.showToast({
+       title: 'Allow permission to be granted',
+     })
+    }
   },
   获取用户信息: function () {
     var that = this;
-    wx.showLoading({ title: '正在登录中' })
+    wx.showLoading({ title: 'Logging in' })
     wx.login({
       success: function (res) {
         if (res.code) {
@@ -26,16 +52,50 @@ Page({
                 AvatarUrl: app.globalData.userInfo.avatarUrl,
                 NiceName: app.globalData.userInfo.nickName
               })
-              wx.hideLoading();
-              //that.AutoLogin(res);      //自动登陆
+              that.AutoLogin(res);      //自动登陆
             },
-            fail: function () { wx.showToast({ title: "获取信息失败!", }) }
+            fail: function () { 
+              that.setData({ UsePermission: false })
+            }
           })
         }
       }
     })
   },
+  //-----自动登陆-----//
+  AutoLogin: function (res) {
+    //----HTTP登陆请求-----//
+    wx.request({
+      url: getApp().globalData.HomeUrl + getApp().globalData.LoginUrl,
+      data: { 
+        "js_code": getApp().globalData.code,
+        "nickname": res.userInfo.nickName, 
+        "gender": res.userInfo.gender, 
+        "province": res.userInfo.province, 
+        "city": res.userInfo.city, 
+        "avatarUrl": res.userInfo.avatarUrl
+      },
+      method: 'POST',
+      success: function (Ares) {
+        wx.hideLoading();
+        console.log(Ares.data);
+        if (Ares.statusCode == 200) {
+          //---登陆成功----//
+          getApp().globalData.user_id = Ares.data.user.userId;
+          console.log("UserId=" + getApp().globalData.user_id);
+        }
+        else {
+          wx.showModal({
+            title: 'Login failure',
+            content: 'Interface return：' + Ares.data,
+          })
+        }
+      },
+      fail: function () { wx.hideLoading(); wx.showToast({ title: 'Login failure'})}
+    })
+  },
   点击开门:function(e){
+    var that = this;
     wx.showModal({
       title: 'Open the door?',
       content: 'Please confirm whether to open the door and settle',
@@ -44,11 +104,33 @@ Page({
       success:function(res){
         if(res.confirm){
           //用户点击确认
-          wx.showToast({
-            title: 'Welcome!',
-          })
+          that.开门请求();
         }
       }
+    })
+  },
+  开门请求:function(){
+    //----HTTP开门请求-----//
+    wx.showLoading({ title: 'Uploading' })
+    wx.request({
+      url: getApp().globalData.HomeUrl + getApp().globalData.OpenDoorUrl,
+      data: {"userId": getApp().globalData.user_id},
+      method: 'POST',
+      success: function (Ares) {
+        wx.hideLoading();
+        console.log(Ares.data);
+        if (Ares.statusCode == 200) {
+          //---开门成功----//
+          wx.showToast({title: 'Door is open'})
+        }
+        else {
+          wx.showModal({
+            title: 'Open failure',
+            content: 'Interface return：' + Ares.data,
+          })
+        }
+      },
+      fail: function () { wx.hideLoading(); wx.showToast({ title: 'Login failure' }) }
     })
   }
 })
